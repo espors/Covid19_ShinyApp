6814833b9306e234a27361fabcc78db8e76aaac3
espors <emma.spors@jacks.sdstate.edu>
2021-08-30 16:10:30 -0500

    updated changes

--- base_code.R
+++ base_code.R
@@ -15,11 +15,21 @@ library(zoo)
 # Import Data ---------
 
 covid_counties <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+covid_states <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 
 county <- read_csv("co-est2020-alldata.csv")
 
 
 # Data Preparation -------
+
+# dataframe for every date from the earliest
+# to most current date 
+earliest <- min(covid_counties$date)
+latest <- max(covid_counties$date)
+dates <- as.data.frame(as_date(earliest:latest))
+
+# all unique county FIPS codes 
+unique_fips <- as.data.frame(unique(covid_counties$fips))
 
 county_pop <- county %>%
   select(SUMLEV, REGION, DIVISION, STATE, COUNTY, STNAME,
