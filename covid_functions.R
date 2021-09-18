46ffaf3a0ff3d027ecad063cb690ff3cf993c595
espors <emma.spors@jacks.sdstate.edu>
2021-09-18 16:45:51 -0500

    Start of graphs
    -updated functions
    -created graphs
    -updated state population to include fips

--- covid_functions.R
+++ covid_functions.R
@@ -17,16 +17,25 @@ covid_rates <- function(covid_data, population){
   return(covid_matrix)
   }
 
-us_rates <- function(county_covid, us_population){
-  county_covid <- county_covid %>%
-    select(-c(fips))
-  county_covid <- na_replace(county_covid,0)
-  us_covid <- county_covid %>%
-    summarise(cases = sum(cases), deaths = sum(deaths))
+us_rates <- function(state_covid, us_population){
+  earliest <- min(state_covid$date)
+  latest <- max(state_covid$date)
+  dates <- as.data.frame(as_date(earliest:latest))
+  dates <- dates %>%
+    rename(date = `as_date(earliest:latest)`)
+  date_matrix <- crossing(us_population, dates)
+  us_covid <- state_covid %>%
+    group_by(date) %>%
+    summarise(cases = sum(cases), 
+              deaths = sum(cases))
   
-  us_covid <- crossing(us_covid, population)
-  us_covid <- us_covid %>%
-    mutate(rateCases = cases/population*100000, 
-           rateDeaths = deaths/population*100000)
+  covid_matrix <- left_join(date_matrix, us_covid, by = c("date"))
+  covid_matrix[,c("cases", "deaths")] <- na_replace(covid_matrix[,c("cases", "deaths")], 0)
+  
+  covid_matrix$cases = covid_matrix$cases/covid_matrix$population * 100000
+  
+  covid_matrix$deaths = covid_matrix$deaths/covid_matrix$population * 100000
+  
+  return(covid_matrix)
 }
 
