3e9aa601d90f302af7d8f8657de14d1f9c8e4056
espors <emma.spors@jacks.sdstate.edu>
2021-09-18 15:42:27 -0500

    Update base_code.R

--- base_code.R
+++ base_code.R
@@ -6,8 +6,6 @@
   
 # Necessary Libraries ----------
 
-
-
 library(readr)
 library(tidyverse)
 library(ggplot2)
@@ -23,7 +21,7 @@ covid_states <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-dat
 
 county_population <- read_csv("county_population.csv")
 state_population <- read_csv("state_population.csv")
-united_states_pop <- read_csv("united_states_pop.csv")
+united_states_pop <- read_csv("united_states_pop")
 
 
 
@@ -47,7 +45,6 @@ county_pop <- county %>%
 test <- crossing(county_population, dates)
 
 county_rates <- covid_rates(covid_counties, county_population)
-state_rates <- covid_rates(covid_states, state_population)
 
 covid_data <- covid_counties
 population <- county_population
@@ -58,8 +55,6 @@ dates <- as.data.frame(as_date(earliest:latest))
 dates <- dates %>%
   rename(date = `as_date(earliest:latest)`)
 date_matrix <- crossing(population, dates)
-covid_data <- covid_data %>%
-  select(-c(fips))
 covid_matrix <- left_join(date_matrix, covid_data, by = c("state", "county", "date"))
 covid_matrix <- na_replace(covid_matrix,0)
 
@@ -74,23 +69,3 @@ covid_matrix$deaths = covid_matrix$deaths/covid_matrix$population * 100000
 covid_matrix$cumulativeCases = covid_matrix$cumulativeCases/covid_matrix$population * 100000
 covid_matrix$cumulativeDeaths = covid_matrix$cumulativeDeaths/covid_matrix$population * 100000
 
-
-covid_rates(covid_counties, county_population)
-us_rates <- us_rates(covid_counties, united_states_pop)
-
-covid_counties <- na_replace(covid_counties,0)
-
-us_covid <- covid_counties %>%
-  summarise(deaths = sum(deaths),cases = sum(cases))
-
-county_covid <- covid_counties
-population <- united_states_pop
-
-county_covid <- na_replace(county_covid,0)
-us_covid <- county_covid %>%
-  summarise(cases = sum(cases), deaths = sum(deaths))
-
-us_covid <- crossing(us_covid, population)
-us_covid <- us_covid %>%
-  mutate(rateCases = cases/population*100000, 
-         rateDeaths = deaths/population*100000)
